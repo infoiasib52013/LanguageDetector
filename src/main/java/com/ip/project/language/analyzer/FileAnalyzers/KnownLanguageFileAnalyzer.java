8169bc0cf34bb8be1c5ff06fe4f4fc73ac23cbe1
infoiasib52013 <infoiasiB52013@gmail.com>
2015-04-28 12:53:52 -0700

    Test commit
    test to show github functionality

--- src/main/java/com/ip/project/language/analyzer/FileAnalyzers/KnownLanguageFileAnalyzer.java
+++ src/main/java/com/ip/project/language/analyzer/FileAnalyzers/KnownLanguageFileAnalyzer.java
@@ -2,10 +2,14 @@ package com.ip.project.language.analyzer.FileAnalyzers;
 
 import com.ip.project.language.analyzer.cryteria.CriteriaMatcher;
 
+import java.nio.file.Path;
+
 /**
  *
  */
 public class KnownLanguageFileAnalyzer implements FileAnalyzer<CriteriaMatcher> {
+
+    private Path filePath;
 
     public void analyzeFile() {
     }
