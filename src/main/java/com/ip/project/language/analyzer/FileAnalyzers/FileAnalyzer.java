bf2581a8931660537079564d92502f89d3fefa89
Ciprian Rosculescu <Ciprian.Rosculescu@pure360.com>
2015-04-28 11:33:31 +0300

    Updated skeleton

--- src/main/java/com/ip/project/language/analyzer/FileAnalyzers/FileAnalyzer.java
+++ src/main/java/com/ip/project/language/analyzer/FileAnalyzers/FileAnalyzer.java
@@ -3,11 +3,8 @@ package com.ip.project.language.analyzer.FileAnalyzers;
 import com.ip.project.language.analyzer.cryteria.CriteriaMatcher;
 
 /**
- *
+ * Created by.
  */
-public class KnownLanguageFileAnalyzer implements FileAnalyzer<CriteriaMatcher> {
-
-    public void analyzeFile() {
-    }
-
+public interface FileAnalyzer<T extends CriteriaMatcher> {
+    void analyzeFile();
 }
---
+++ src/main/java/com/ip/project/language/analyzer/cryteria/CriteriaMatcher.java
@@ -0,0 +1,7 @@
+package com.ip.project.language.analyzer.cryteria;
+
+/**
+ * Created by .
+ */
+public class CriteriaMatcher<T extends Criteria> {
+}
